1a9e998c9440bd35c0cbdfde637a4eb0ac293f03
Frankie Sadler <frankie.sadler11@gmail.com>
2020-11-08 11:55:53 +0000

    Cleaned create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">PLACEHOLDER</header>
+    </div>
+  );
+}
+
+export default App;
