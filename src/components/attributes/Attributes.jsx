b949539016f3b00471bf6664b93e3a3453f46467
Frankie Sadler <frankie.sadler11@gmail.com>
2020-11-08 18:45:10 +0000

    Created more attribute applications

--- src/components/attributes/Attributes.jsx
+++ src/components/attributes/Attributes.jsx
@@ -1,6 +1,9 @@
 import AttributeScores from "./AttributeScores";
 import SavingThrows from "./SavingThrows";
 import SkillScores from "./SkillScores";
+import OtherProficiencies from "./OtherProficiencies";
+import PassivePerception from "./PassivePerception";
+import Inspiration from "./Inspiration";
 
 const Attributes = (props) => {
   return (
@@ -8,32 +11,14 @@ const Attributes = (props) => {
       <section className="attributes">
         <AttributeScores />
         <div className="attr-applications">
-          <div className="inspiration box">
-            <div className="label-container">
-              <label htmlFor="inspiration">Inspiration</label>
-            </div>
-            <input name="inspiration" type="checkbox" />
-          </div>
-          <div className="proficiencybonus box">
-            <div className="label-container">
-              <label htmlFor="proficiencybonus">Proficiency Bonus</label>
-            </div>
-            <input name="proficiencybonus" placeholder="+2" />
-          </div>
+          <Inspiration />
+          <PassivePerception />
           <SavingThrows />
           <SkillScores />
         </div>
       </section>
-      <div className="passive-perception box">
-        <div className="label-container">
-          <label htmlFor="passiveperception">Passive Wisdom (Perception)</label>
-        </div>
-        <input name="passiveperception" placeholder="10" />
-      </div>
-      <div className="otherprofs box textblock">
-        <label htmlFor="otherprofs">Other Proficiencies and Languages</label>
-        <textarea name="otherprofs"></textarea>
-      </div>
+      <PassivePerception />
+      <OtherProficiencies />
     </section>
   );
 };
---
+++ src/components/attributes/Inspiration.jsx
@@ -0,0 +1,12 @@
+const Inspiration = (props) => {
+  return (
+    <div className="inspiration box">
+      <div className="label-container">
+        <label htmlFor="inspiration">Inspiration</label>
+      </div>
+      <input name="inspiration" type="checkbox" />
+    </div>
+  );
+};
+
+export default Inspiration;
