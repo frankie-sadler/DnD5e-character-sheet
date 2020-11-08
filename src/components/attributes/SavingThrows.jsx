aa9e932e87b3ef0dbcfc6371dfe0664b298febc4
Frankie Sadler <frankie.sadler11@gmail.com>
2020-11-08 18:11:15 +0000

    Created SavingThrowBox

--- src/components/attributes/SavingThrows.jsx
+++ src/components/attributes/SavingThrows.jsx
@@ -1,37 +1,15 @@
+import SavingThrowBox from "./SavingThrowBox";
+
 const SavingThrows = (props) => {
   return (
     <div className="saves list-section box">
       <ul>
-        <li>
-          <label htmlFor="Strength-save">Strength</label>
-          <input name="Strength-save" placeholder="+0" type="text" />
-          <input name="Strength-save-prof" type="checkbox" />
-        </li>
-        <li>
-          <label htmlFor="Dexterity-save">Dexterity</label>
-          <input name="Dexterity-save" placeholder="+0" type="text" />
-          <input name="Dexterity-save-prof" type="checkbox" />
-        </li>
-        <li>
-          <label htmlFor="Constitution-save">Constitution</label>
-          <input name="Constitution-save" placeholder="+0" type="text" />
-          <input name="Constitution-save-prof" type="checkbox" />
-        </li>
-        <li>
-          <label htmlFor="Wisdom-save">Wisdom</label>
-          <input name="Wisdom-save" placeholder="+0" type="text" />
-          <input name="Wisdom-save-prof" type="checkbox" />
-        </li>
-        <li>
-          <label htmlFor="Intelligence-save">Intelligence</label>
-          <input name="Intelligence-save" placeholder="+0" type="text" />
-          <input name="Intelligence-save-prof" type="checkbox" />
-        </li>
-        <li>
-          <label htmlFor="Charisma-save">Charisma</label>
-          <input name="Charisma-save" placeholder="+0" type="text" />
-          <input name="Charisma-save-prof" type="checkbox" />
-        </li>
+        <SavingThrowBox attributeName="Strength" />
+        <SavingThrowBox attributeName="Dexterity" />
+        <SavingThrowBox attributeName="Constitution" />
+        <SavingThrowBox attributeName="Wisdom" />
+        <SavingThrowBox attributeName="Intelligence" />
+        <SavingThrowBox attributeName="Charisma" />
       </ul>
       <div className="label">Saving Throws</div>
     </div>
