9a5a113bac8bd2cc963cf791da24fd98467f443d
Frankie Sadler <frankie.sadler11@gmail.com>
2020-12-03 21:38:02 +0000

    Created store - Installed redux and extras - Created store - Added provider to index.js - Created reducers - Created actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
+import store from "./store";
 
 import "./styles/index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
